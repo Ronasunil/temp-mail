f94d60180df7a8359ce071105ca76d0e2e313675
Adnan <adnanxyz06@gmail.com>
2026-03-27 12:00:38 +0530

    chore: redirect TypeScript build output to build folder

--- src/apis/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_route_1 = require("./auth/auth.route");
-const router = (0, express_1.Router)();
-// Auth routes
-router.use("/auth", auth_route_1.authRouter);
-exports.default = router;
--- src/packages/shared/config/env.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AppEnv {
-}
-AppEnv.SMTP_SERVER_PORT = +process.env.SMTP_SERVER_PORT;
-exports.default = AppEnv;
